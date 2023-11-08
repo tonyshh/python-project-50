97189a9ff9c1923527b40c1db04e5972260096ef
tonyshh <shalin.anton8@gmail.com>
2023-11-08 23:01:35 +0700

    step_2

--- hexlet_code/gendiff.py
+++ hexlet_code/gendiff.py
@@ -2,9 +2,12 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', help='first file to compare')
-    parser.add_argument('second_file', help='second file to compare')
+    parser.add_argument('first_file', help='first configuration file')
+    parser.add_argument('second_file', help='second configuration file')
+    parser.add_argument('-f', '--format', help='set format of output', default='stylish')
+
     args = parser.parse_args()
+    
 
 if __name__ == '__main__':
     main()
