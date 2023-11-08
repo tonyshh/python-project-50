b593f183afedabadca676ea5f3211f7c9347fc53
tonyshh <shalin.anton8@gmail.com>
2023-11-08 23:17:04 +0700

    step_3 (no asciinema)

--- hexlet_code/gendiff.py
+++ hexlet_code/gendiff.py
@@ -1,4 +1,25 @@
 import argparse
+import json
+
+def generate_diff(file_path1, file_path2):
+    file1_data = json.load(open(file_path1))
+    file2_data = json.load(open(file_path2))
+
+    diff = []
+    all_keys = sorted(file1_data.keys() | file2_data.keys())
+
+    for key in all_keys:
+        if key in file1_data and key not in file2_data:
+            diff.append(f"  - {key}: {file1_data[key]}")
+        elif key not in file1_data and key in file2_data:
+            diff.append(f"  + {key}: {file2_data[key]}")
+        elif file1_data[key] == file2_data[key]:
+            diff.append(f"    {key}: {file1_data[key]}")
+        else:
+            diff.append(f"  - {key}: {file1_data[key]}")
+            diff.append(f"  + {key}: {file2_data[key]}")
+
+    return '{\n' + '\n'.join(diff) + '\n}'
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
@@ -7,7 +28,8 @@ def main():
     parser.add_argument('-f', '--format', help='set format of output', default='stylish')
 
     args = parser.parse_args()
-    
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 if __name__ == '__main__':
     main()
